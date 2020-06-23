1493a9273f297635339901f6bf21da9a5ec85cad
alexp <pirogovski@gmail.com>
2020-06-23 11:11:08 +0300

    With large items

--- plays/roles/setup_data_nodes/files/stats_reader.py
+++ plays/roles/setup_data_nodes/files/stats_reader.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python
 
 import sys
-import subprocess
+import json
+import argparse
 
 class StatsReader(object):
-    def __init__(self):
-        self.stats = {}
-        self._cmd = '/home/iguazio//igz/engine/node_runner/bin/stats_reader -f /dev/shm'
-        self.raw_stats = self.run_cmd(self._cmd)
+    def __init__(self, stats_file):
+        with open(stats_file, 'r') as f:
+            self.stats = json.load(f)
+        self.combined_stats_dict = {
+                                        'cache_hits': 0,
+                                        'cache_miss': 0,
+                                        'cache_d_reads': 0,
+                                        'cache_io_pages': 0,
+                                        'cache_d_saves': 0
+                                   }
 
-    @staticmethod
-    def run_cmd(cmd, exit_on_fail=True):
-        try:
-            # logger.debug('Executing: {}'.format(cmd))
-            return subprocess.check_output(cmd.split(' ')).split('\n')
-        except subprocess.CalledProcessError as e:
-            # logger.error('Command \"{}\" exited with RC={}, message: {}'.format(cmd, e.returncode, e.output))
-            print('ERROR: %s, RC=%s', e.output, e.returncode)
-            if exit_on_fail:
-                sys.exit(1)
-            return None
+    def aggregate_stats(self):
+        for key in self.stats.keys():
+            if 'cache' in key:
+                for cache_key in self.stats[key]:
+                    # print(self.stats[key][cache_key]['counts'])
+                    self.combined_stats_dict[cache_key] += self.stats[key][cache_key]['counts']
 
-    # def build_stats_dict(self, raw_output):
-    #     return ''
+    def print_combined_stats(self):
+        print(json.dumps(self.combined_stats_dict, indent=4))
+
+def _parse_cli():
+    parser = argparse.ArgumentParser(description='Feed file name to script')
+    parser.add_argument('-f', '--file', help='Stats_reader results JSON', required=True)
+    return parser.parse_args()
+
+def main():
+    args = _parse_cli()
+    print('Combined results for {0}'.format(args.file))
+    reader = StatsReader(args.file)
+    reader.aggregate_stats()
+    reader.print_combined_stats()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+
 
