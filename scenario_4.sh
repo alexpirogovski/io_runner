9756060e1564be78f3733b968565bb8d1ef9623d
alexp <pirogovski@gmail.com>
2020-10-26 14:21:28 +0200

    Fixes

--- scenario_4.sh
+++ scenario_4.sh
@@ -2,4 +2,4 @@
 
 #export ANSIBLE_COW_SELECTION=elephant
 export ANSIBLE_NOCOWS=1
-ansible-playbook -i inventory/hosts.ini plays/oos_put_read.yml
+ansible-playbook -i inventory/hosts.ini plays/oos_obj_put_get.yml
